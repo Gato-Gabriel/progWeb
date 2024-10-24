c128a9fec914d315a224e1938b9c05317b13018a
feitosa-16 <heloisafeitosarocha3495@gmail.com>
2024-10-24 14:15:14 -0300

    Correções de dropdown e slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let slideIndex = 0;
 
 function moverCarrossel(n) {
-    const carrossel = document.querySelector('.carrossel');
-    const cards = document.querySelectorAll('.carrossel .card');
-    const totalCards = cards.length;
+    const carrossel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.carousel-slide');
+    const totalCards = slides.length;
 
     slideIndex += n;
 
@@ -15,6 +15,37 @@ function moverCarrossel(n) {
 
     carrossel.style.transform = `translateX(${-slideIndex * 100}%)`;
 }
+
+    function toggleCard(button) {
+        const card = button.parentElement;
+        const fullText = card.querySelector('.full');
+        const isVisible = fullText.style.display === 'block';
+    
+        if (isVisible) {
+            fullText.style.display = 'none';
+            button.textContent = 'Leia Mais';
+        } else {
+            fullText.style.display = 'block';
+            button.textContent = 'Mostrar Menos';
+        }
+    }
+    
+    function toggleCard(cardHeader) {
+        const card = cardHeader.parentElement;
+        const fullText = card.querySelector('.full');
+        const isVisible = fullText.style.display === 'block';
+    
+        if (isVisible) {
+            fullText.style.display = 'none';
+        } else {
+            fullText.style.display = 'block';
+        }
+    }
+    
+
+    
+
+
 
 // Menu Hamburguer
 const hamMenu = document.querySelector(".hamburger_menu");
