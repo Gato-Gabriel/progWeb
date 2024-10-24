43934975c3fddb87481e29146c43974b9292aa7a
Gabriel Gato <70043907+Gato-Gabriel@users.noreply.github.com>
2024-10-24 16:50:22 -0300

    Versão Final 2a entrega

--- js/script.js
+++ js/script.js
@@ -16,34 +16,56 @@ function moverCarrossel(n) {
     carrossel.style.transform = `translateX(${-slideIndex * 100}%)`;
 }
 
-    function toggleCard(button) {
-        const card = button.parentElement;
-        const fullText = card.querySelector('.full');
-        const isVisible = fullText.style.display === 'block';
-    
-        if (isVisible) {
-            fullText.style.display = 'none';
-            button.textContent = 'Leia Mais';
-        } else {
-            fullText.style.display = 'block';
-            button.textContent = 'Mostrar Menos';
-        }
-    }
-    
-    function toggleCard(cardHeader) {
-        const card = cardHeader.parentElement;
-        const fullText = card.querySelector('.full');
-        const isVisible = fullText.style.display === 'block';
-    
-        if (isVisible) {
-            fullText.style.display = 'none';
-        } else {
-            fullText.style.display = 'block';
-        }
-    }
-    
+function toggleCardBtn(button) {    // Cards com o "leia mais"
+    const card = button.parentElement;
+    const fullText = card.querySelector('.full');
+    const isVisible = fullText.style.display === 'block';
 
+    if (isVisible) {
+        fullText.style.display = 'none';
+        button.textContent = 'Leia Mais';
+    } else {
+        fullText.style.display = 'block';
+        button.textContent = 'Mostrar Menos';
+    }
+}
+
+function toggleCard(cardHeader) {   // Cards com dropdown
+    const card = cardHeader.parentElement;
+    const fullText = card.querySelector('.full');
+    const cardIcon = card.querySelector('i');
+    const isVisible = fullText.style.display === 'block';
+
+
+    if (isVisible) {
+        fullText.style.display = 'none';
+        cardIcon.className = "fa-solid fa-caret-down";
+    } else {
+        fullText.style.display = 'block';
+        cardIcon.className = "fa-solid fa-caret-up";
+    }
+}
+
+// Cria a NAV dinamicamente em todas as páginas
+function dinamicNav(){
+    const menuItems = [
+        { href: './home.html', icon: 'fa-house-chimney', label: 'Home' },
+        { href: './orgs.html', icon: 'fa-people-group', label: 'Organizações' },
+        { href: './alunos.html', icon: 'fa-graduation-cap', label: 'Alunos' },
+        { href: './instituicao.html', icon: 'fa-school', label: 'Instituição' },
+        { href: './divulgacoes.html', icon: 'fa-bullhorn', label: 'Divulgações' },
+        { href: './contato.html', icon: 'fa-address-book', label: 'Contato' }
+    ];
     
+    const menuContainer = document.getElementById('menu');
+    
+    menuItems.forEach(item => {
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="${item.href}"><i class="fa-solid ${item.icon}"></i>&nbsp;${item.label}</a>`;
+        menuContainer.appendChild(li);
+    });
+}
+dinamicNav();
 
 
 
